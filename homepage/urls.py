62f50a58c56cedd20d5af254b3e0681ac5a56737
Gabrielle S Andrade <gsandrade.00@gmail.com>
2024-03-03 13:38:18 -0300

    add templates config

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,7 @@
-# from django.urls import path, include
-# from .views import homepage
+from django.urls import path, include
+from .views import homepage
 
-# urlpatterns = [
-#     path('', include('homepage.urls')),
-#     # path('', homepage) #creating homepage, there is no / inside this path
-# ]
+urlpatterns = [
+    #path('', include('homepage.urls')),
+    path('', homepage) #creating homepage, there is no / inside this path
+]
